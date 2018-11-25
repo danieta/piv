94299a76e3992d18b03d8e5b6b0985d96b3f906b
MartimBraga <martim.gard.braga@tecnico.ulisboa.pt>
2018-11-25 19:33:54 +0000

    PointCloud
    Point cloud with Foreground isolated

--- depth_background.m
+++ depth_background.m
@@ -16,8 +16,6 @@ for i=1:length(images_jpg),
     title("Depth");
 end
 
-
-
 cd ..;
 %% Computing the background for the depth and RGB image sequences given
 bg_depth=median(images_depth,3);
@@ -60,21 +58,44 @@ end
 %% Testing the pointcloud generation
 
 %which image to generate pointcloud of
-image_nr = 7;
+image_nr = 5;
+% figure(4);
+% imagesc(foreground_depth_morphed(:,:,image_nr));
+% title("Foreground morphed same as pC");
+% figure(5);
+% imagesc(images_depth(:,:,image_nr));
+% title("Depth same as pC");
+%finding to blob in the filtered image
+index_filtered_to_pc = find(foreground_depth_morphed(:,:,image_nr)== 1);
 
 %corresponding depth foreground and rgb of this image
-imd = foreground_depth_morphed(:,:,image_nr);
+imd = images_depth(:,:,image_nr);
 im = double(images_rgb(:,:,:,image_nr));
 
 %reshaping the foreground depth image so it is a long vector instead of
 % being a matrix. this is just so that we can do fast computations in 
 % matlab
+
 foreground_reshaped=reshape(double(imd),[],1);
 Z=double(foreground_reshaped');
 
 %dont really know whats happening here
 [v u]=ind2sub([480 640],(1:480*640));
 P=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z];
+
+
+%deletes the background from the PC
+a=1;
+for i=1:length(P)
+    if i==index_filtered_to_pc(a)
+        if a < length(index_filtered_to_pc)
+            a=a+1;
+        end
+    else 
+        P(:, i)=[0 0 0];
+    end
+end
+
 niu=cam_params.Krgb*[cam_params.R cam_params.T]*[P;ones(1,640*480)];
 u2=round(niu(1,:)./niu(3,:));
 v2=round(niu(2,:)./niu(3,:));
@@ -83,6 +104,10 @@ v2=round(niu(2,:)./niu(3,:));
 im2=zeros(640*480,3);
 indsclean=find((u2>=1)&(u2<=641)&(v2>=1)&(v2<=480));
 indscolor=sub2ind([480 640],v2(indsclean),u2(indsclean));
+
+%error we were passing im = double(images_rgb(:,:,:,image_nr));
+%so the image was without depth 
+%corrected with im_pC
 im1aux=reshape(im,[640*480 3]);
 im2(indsclean,:)=im1aux(indscolor,:);
 
