54a19b6cc492b58fa34f67cfcc08a95a23014f82
danieta <31984540+danieta@users.noreply.github.com>
2018-11-26 12:19:46 +0000

    added bwlabel to pointcloud generation

--- depth_background.m
+++ depth_background.m
@@ -7,66 +7,56 @@ images_mat=dir('*.mat');
 
 images_depth=zeros(480,640,length(images_jpg));
 
-for i=1:length(images_jpg),
-    images_rgb(:,:,:,i)=(imread(images_jpg(i).name));
-    load(images_mat(i).name);
-    images_depth(:,:,i)=double(depth_array)/1000;
-    figure(1);
-    imagesc(images_depth(:,:,i));
-    title("Depth");
+for frame_number=1:length(images_jpg)
+    images_rgb(:,:,:,frame_number)=(imread(images_jpg(frame_number).name));
+    load(images_mat(frame_number).name);
+    images_depth(:,:,frame_number)=double(depth_array)/1000;
 end
 
+number_of_frames = frame_number;
+
 cd ..;
-%% Computing the background for the depth and RGB image sequences given
+%% Computing the background for the depth sequences given
 bg_depth=median(images_depth,3);
 figure(2);
 imagesc(bg_depth);
 title("depth background");
 
+filter_criteria = strel('disk',7);
 
-figure(1);clf;
-
-filter_criteria = strel('disk',10);
-
-for i=1:length(images_jpg),
+for frame_number=1:length(images_jpg),
     %subtract current image and depth background
-    foreground_depth(:,:,i)=abs(images_depth(:,:,i)-bg_depth)>.50;
+    foreground_depth(:,:,frame_number)=abs(images_depth(:,:,frame_number)-bg_depth)>.50;
     
     %morph filter every depth image. remove noise components
-    foreground_depth_morphed(:,:,i)=imopen(foreground_depth(:,:,i),filter_criteria);
+    foreground_depth_morphed(:,:,frame_number)=imopen(foreground_depth(:,:,frame_number),filter_criteria);
     
     
     figure(1);
-    imagesc(foreground_depth_morphed(:,:,i));
+    imagesc(foreground_depth_morphed(:,:,frame_number));
     title("Foreground morphed");
     
 %     figure(4);
-%     %[labels, number_of_objects] = bwlabel(depth_subtracted(:,:,i));
-%     labels = regionprops(foreground_depth_morphed(:,:,i), 'centroid');
-%     centroids = cat(1, labels.Centroid);
-%     imshow(foreground_depth_morphed(:,:,i));
+%     [labels, number_of_objects] = bwlabel(foreground_depth(:,:,frame_number));
+%     labels = regionprops(foreground_depth_morphed(:,:,frame_number));
+%     centroids = cat(1, labels.BoundingBox);
+%     imshow(foreground_depth_morphed(:,:,frame_number));
 %     hold on;
 %     plot(centroids(:,1),centroids(:,2), 'b*')
 %     hold off;
 %     title("bwlabel test");
-%     
-%     figure(5);
-%     imshow(imdilate(foreground_depth_morphed(:,:,i), filter_criteria));
-%     title("dilated");
 end
+
 
 %% Testing the pointcloud generation
 
 %which image to generate pointcloud of
 image_nr = 5;
-% figure(4);
-% imagesc(foreground_depth_morphed(:,:,image_nr));
-% title("Foreground morphed same as pC");
-% figure(5);
-% imagesc(images_depth(:,:,image_nr));
-% title("Depth same as pC");
-%finding to blob in the filtered image
-index_filtered_to_pc = find(foreground_depth_morphed(:,:,image_nr)== 1);
+
+%Use bwlabel to plot the elements in 'labels' that are not 0 (that are not
+% background objects)
+[labels, number_of_objects] = bwlabel(foreground_depth_morphed(:,:,image_nr));
+index_filtered_to_pc = find(foreground_depth_morphed(:,:,image_nr) ~= 0);
 
 %corresponding depth foreground and rgb of this image
 imd = images_depth(:,:,image_nr);
@@ -81,18 +71,18 @@ Z=double(foreground_reshaped');
 
 %dont really know whats happening here
 [v u]=ind2sub([480 640],(1:480*640));
-P=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z];
+P=inv(cam_params.Kdepth)*[Z.*u; Z.*v; Z];
 
 
 %deletes the background from the PC
 a=1;
-for i=1:length(P)
-    if i==index_filtered_to_pc(a)
+for frame_number=1:length(P)
+    if frame_number==index_filtered_to_pc(a)
         if a < length(index_filtered_to_pc)
             a=a+1;
         end
     else 
-        P(:, i)=[0 0 0];
+        P(:, frame_number)=[0 0 0];
     end
 end
 
